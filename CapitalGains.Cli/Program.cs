5da048081b4bcfd999489cec407618f76cf786ec
Thiago Oliveira <thiagothecrazy@gmail.com>
2025-07-14 16:13:20 -0300

    Add estrutura de DI

--- CapitalGains.Cli/Program.cs
+++ CapitalGains.Cli/Program.cs
@@ -1,5 +1,8 @@
+using CapitalGains.App.Interfaces;
 using CapitalGains.App.Services;
 using CapitalGains.Cli.Infrastructure;
+using CapitalGains.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
 namespace CapitalGains.Cli;
@@ -10,12 +13,28 @@ class Program
     {
         LoggingSetup.Configure();
         using var tracerProvider = TracingSetup.Configure();
-
         var logger = Log.Logger;
-        var calculator = new CapitalGainsCalculator();
-        
+
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        var provider = services.BuildServiceProvider();
+                
+        var calculator = provider.GetRequiredService<ICapitalGainsCalculator>();
         var runner = new StdInRunner(calculator, logger);
 
         runner.Run();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<Portfolio>();
+
+        // Handlers
+        //services.AddScoped<ITradeOperationHandler, BuyOperationHandler>();
+        //services.AddScoped<ITradeOperationHandler, SellOperationHandler>();
+        //services.AddScoped<ITradeOperationHandler, DividendOperationHandler>();
+
+        // Calculator depende dos handlers
+        services.AddScoped<ICapitalGainsCalculator, CapitalGainsCalculator>();
     }
 }
